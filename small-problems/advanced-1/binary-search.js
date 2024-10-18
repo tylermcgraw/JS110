4592bf5910867d5dc2bbd62f7950c791c9d0c35d
tylermcgraw <=>
2024-10-17 21:42:53 -0400

    binary search

---
+++ small-problems/advanced-1/binary-search.js
@@ -0,0 +1,24 @@
+function binarySearch(array, searchItem) {
+  let lower = 0;
+  let upper = array.length - 1;
+  while (upper !== lower) {
+    let avg = (upper + lower) / 2;
+    if (array[avg] > searchItem) {
+      upper -= (upper - avg) || 1;
+    } else {
+      lower += (avg - lower) || 1;
+    }
+  }
+  return array[upper] === searchItem ? upper : -1;
+}
+
+let yellowPages = ['Apple Store', 'Bags Galore', 'Bike Store', 'Donuts R Us', 'Eat a Lot', 'Good Food', 'Pasta Place', 'Pizzeria', 'Tiki Lounge', 'Zooper'];
+console.log(binarySearch(yellowPages, 'Pizzeria'));                   // 7
+console.log(binarySearch(yellowPages, 'Apple Store'));                // 0
+
+console.log(binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 77));    // -1
+console.log(binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 89));    // 7
+console.log(binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 5));     // 1
+
+console.log(binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Peter'));  // -1
+console.log(binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Tyler'));  // 6
