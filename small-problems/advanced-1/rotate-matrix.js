b390e6a06bf2df2df79a87a5d3b3294b7b33617b
tylermcgraw <=>
2024-10-17 20:57:15 -0400

    advanced matrix problems

---
+++ small-problems/advanced-1/rotate-matrix.js
@@ -0,0 +1,29 @@
+function rotate90(matrix) {
+  let rotatedMatrix = [];
+  matrix[0].forEach(_ => rotatedMatrix.push([]));
+  matrix.forEach(row => {
+    row.forEach((num, idx) => {
+      rotatedMatrix[idx].unshift(num);
+    });
+  });
+  return rotatedMatrix;
+}
+
+let matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6],
+];
+
+let matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8],
+];
+
+let newMatrix1 = rotate90(matrix1);
+let newMatrix2 = rotate90(matrix2);
+let newMatrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))));
+
+console.log(newMatrix1);      // [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+console.log(newMatrix2);      // [[5, 3], [1, 7], [0, 4], [8, 2]]
+console.log(newMatrix3);      // `matrix2` --> [[3, 7, 4, 2], [5, 1, 0, 8]]
---
+++ small-problems/advanced-1/transpose-matrix.js
@@ -0,0 +1,39 @@
+function transpose (matrix) {
+  let transposedMatrix = [];
+  matrix[0].forEach(_ => transposedMatrix.push([]));
+  matrix.forEach(row => {
+    row.forEach((num, idx) => {
+      transposedMatrix[idx].push(num);
+    });
+  });
+  return transposedMatrix;
+}
+
+const matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+];
+
+let newMatrix = transpose(matrix);
+
+console.log(newMatrix);      // [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+console.log(matrix);         // [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+
+const matrix2 = [
+  [1, 5, 8, 5],
+  [4, 7, 2, 0],
+  [3, 9, 6, 1]
+];
+
+let newMatrix2 = transpose(matrix2);
+
+console.log(newMatrix2);      // [[1, 4, 3], [5, 7, 9], [8, 2, 6], [5, 0, 1]]
+console.log(matrix2);         // [[1, 5, 8, 5], [4, 7, 2, 0], [3, 9, 6, 1]]
+
+console.log(transpose([[1, 2, 3, 4]]));            // [[1], [2], [3], [4]]
+console.log(transpose([[1], [2], [3], [4]]));      // [[1, 2, 3, 4]]
+console.log(transpose([[1]]));                     // [[1]]
+
+console.log(transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]));
+// [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
